4ee7fa7ae8627304372ffdb8d961fb7b88ec0321
fretzestavillo <estavillofretz@gmail.com>
2024-11-27 17:04:01 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,35 +6,35 @@
 //   plugins: [react()],
 // });
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === "production" ? "/malone-fashion-shop/" : "/", // Set base for GitHub Pages in production
-    build: {
-      // These are optional build options for further optimization during deployment
-      sourcemap: mode === "development", // Generate source maps in development
-      minify: mode === "production", // Minify for production builds
-      rollupOptions: {
-        output: {
-          // Customize chunk names for production if necessary
-          chunkFileNames:
-            mode === "production"
-              ? "assets/[name]-[hash].js"
-              : "assets/[name].js",
-          entryFileNames:
-            mode === "production"
-              ? "assets/[name]-[hash].js"
-              : "assets/[name].js",
-        },
-      },
-    },
-    server: {
-      open: true, // Automatically open browser in dev mode
-      port: 3000, // Define the port for development server
-    },
-  };
-});
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   return {
+//     plugins: [react()],
+//     base: mode === "production" ? "/malone-fashion-shop/" : "/", // Set base for GitHub Pages in production
+//     build: {
+//       // These are optional build options for further optimization during deployment
+//       sourcemap: mode === "development", // Generate source maps in development
+//       minify: mode === "production", // Minify for production builds
+//       rollupOptions: {
+//         output: {
+//           // Customize chunk names for production if necessary
+//           chunkFileNames:
+//             mode === "production"
+//               ? "assets/[name]-[hash].js"
+//               : "assets/[name].js",
+//           entryFileNames:
+//             mode === "production"
+//               ? "assets/[name]-[hash].js"
+//               : "assets/[name].js",
+//         },
+//       },
+//     },
+//     server: {
+//       open: true, // Automatically open browser in dev mode
+//       port: 3000, // Define the port for development server
+//     },
+//   };
+// });
